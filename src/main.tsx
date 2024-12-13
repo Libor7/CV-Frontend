bccc9fb2d6bd467453ca3910bcac084697bd7b5b
Libor Vanek <libor26@gmail.com>
2024-12-13 23:57:35 +0100

    setting paths for nicer import statements in tsconfig file and creating custom theme in material ui, palette and font-family customization

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/** CUSTOM COMPONENTS */
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+/** LIBRARIES */
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+/** STYLES */
+import "./index.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5",
+      light: "#6573c3",
+      dark: "#2c387e",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#009688",
+      light: "#33ab9f",
+      dark: "#00695f",
+      contrastText: "#000",
+    },
+    background: {
+      default: "#f5f5f5",
+    },
+    text: {
+      primary: "#212121",
+    },
+    warning: {
+      main: "#ff5722",
+    },
+  },
+  typography: {
+    fontFamily: ["Arial", "Cambria", "Garamond", "Times New Roman"].join(","),
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
